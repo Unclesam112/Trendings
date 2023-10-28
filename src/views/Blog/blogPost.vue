6e98344e8441435adddfa09eeebf97e486ae8ec1
Engineer Adeyemi <66875174+Unclesam112@users.noreply.github.com>
2023-10-28 15:15:56 -0700

    new

--- src/views/Blog/blogPost.vue
+++ src/views/Blog/blogPost.vue
@@ -53,10 +53,10 @@
                     </ol>
                 </nav>
 
-            </div>
+            </div>__
             <div class="grid grid-cols-1 md:grid-cols-3 gap-8">
-                <div class="col-span-2">
-                    <h1 class="md:text-3xl lg:text-5xl text-2xl pb-4" style="line-height: 50px;">
+                <div class="md:col-span-2 col">
+                    <h1 class="md:text-3xl lg:text-5xl text-2xl pb-4">
                         Lorem ipsum dolor sit, amet consectetur adipisicing elit. Obcaecati impedit velit totam?
                     </h1>
 
@@ -69,20 +69,20 @@
                     </small>
                     <img src="../../assets/img/img.jpg" alt="" class="rounded">
 
-                    <div class="flex items-center space-x-2 my-3">
+                    <div class="flex my-3">
                         <span class="text-gray-600"> 
                             <span class="text-red-700 text-3xl font-bold">0</span>
                             Shares</span>
-                        <a class="bg-blue-500 hover:bg-blue-600 text-white px-3 py-1 rounded flex transition duration-300">
-                            <Icon icon="ri:facebook-fill" class="mt-1"/> <span class="mx-2">Facebook</span>
+                        <a class="bg-blue-500 hover:bg-blue-600 ml-3 text-white px-3 py-1 rounded flex transition duration-300">
+                            <Icon icon="ri:facebook-fill" class="mt-1"/> <span class="mx-2 hidden md:block">Facebook</span>
                         </a>
 
-                        <a class="bg-green-500 hover:bg-green-600 text-white px-3 py-1 rounded flex transition duration-300">
-                            <Icon icon="ic:baseline-whatsapp" class="mt-1"/> <span class="mx-2">Whatsapp</span>
+                        <a class="bg-green-500 hover:bg-green-600 mx-2 text-white px-3 py-1 rounded flex transition duration-300">
+                            <Icon icon="ic:baseline-whatsapp" class="mt-1"/> <span class="mx-2 hidden md:block">Whatsapp</span>
                         </a>
 
                         <a class="bg-blue-400 hover:bg-blue-400 text-white px-3 py-1 rounded flex transition duration-300">
-                            <Icon icon="mdi:twitter" class="mt-1"/> <span class="mx-2"> Twitter</span>
+                            <Icon icon="mdi:twitter" class="mt-1 "/> <span class="mx-2 hidden md:block"> Twitter</span>
                         </a>
                     </div>
                     <div class="text-left my-3">
@@ -106,6 +106,22 @@
                             explicabo quibusdam tempora delectus sapiente optio nostrum placeat ad harum atque. Inventore
                             incidunt debitis eos magni soluta blanditiis, autem iste facere tenetur quo exercitationem?
                         </p>
+                        <div class="flex">
+                        <span class="text-red-600 mr-5"> 
+                          
+                            Shares this:</span>
+                        <a class="bg-blue-500 hover:bg-blue-600 text-white w-8 h-8 rounded-full text-center pt-1 pl-2 transition duration-300">
+                            <Icon icon="ri:facebook-fill" class="mt-1"/>
+                        </a>
+
+                        <a class="bg-green-500 mx-2 hover:bg-green-600 w-8 h-8 text-white rounded-full  pt-1 pl-2 transition duration-300">
+                            <Icon icon="ic:baseline-whatsapp" class="mt-1"/>
+                        </a>
+
+                        <a class="bg-blue-400 hover:bg-blue-400  pt-1 pl-2 w-8 h-8 text-white p rounded-full transition duration-300">
+                            <Icon icon="mdi:twitter" class="mt-1"/>
+                        </a>
+                    </div>
                     </div>
                 </div>
 
@@ -229,7 +245,7 @@
             <div class="widget-3 ">
                 <div class="container">
                     <div class="flex py-1 mb-8 border-l-4 border border-l-blue-500">
-                        <h1 class="text-left md:text-3xl text-lg font-bold pl-2">Popular Posts</h1>
+                        <h1 class="text-left md:text-3xl text-lg font-bold pl-2">Related</h1>
                     </div>
                     <div class="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-4">
                         <div class="col">
@@ -376,6 +392,48 @@
                         </div>
                     </div>
                 </div>
+            </div>
+
+            <div class="comment">
+                <h1 class="text-5xl py-3">
+                    Leave a reply
+                </h1>
+
+<form>
+   <div class="w-full mb-4 border border-gray-200 rounded-lg bg-gray-50 dark:bg-gray-700 dark:border-gray-600">
+       <div class="px-4 py-2 bg-white rounded-t-lg dark:bg-gray-800">
+           <label for="comment" class="sr-only">Your comment</label>
+           <textarea id="comment" rows="4" class="w-full px-0 text-sm text-gray-900 bg-white border-0 dark:bg-gray-800 focus:ring-0 dark:text-white dark:placeholder-gray-400" placeholder="Write a comment..." required></textarea>
+       </div>
+       <div class="flex items-center justify-between px-3 py-2 border-t dark:border-gray-600">
+           <button type="submit" class="inline-flex items-center py-2.5 px-4 text-xs font-medium text-center text-white bg-blue-700 rounded-lg focus:ring-4 focus:ring-blue-200 dark:focus:ring-blue-900 hover:bg-blue-800">
+               Post comment
+           </button>
+           <div class="flex pl-0 space-x-1 sm:pl-2">
+               <button type="button" class="inline-flex justify-center items-center p-2 text-gray-500 rounded cursor-pointer hover:text-gray-900 hover:bg-gray-100 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-600">
+                   <svg class="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 12 20">
+                        <path stroke="currentColor" stroke-linejoin="round" stroke-width="2" d="M1 6v8a5 5 0 1 0 10 0V4.5a3.5 3.5 0 1 0-7 0V13a2 2 0 0 0 4 0V6"/>
+                    </svg>
+                   <span class="sr-only">Attach file</span>
+               </button>
+               <button type="button" class="inline-flex justify-center items-center p-2 text-gray-500 rounded cursor-pointer hover:text-gray-900 hover:bg-gray-100 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-600">
+                   <svg class="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 16 20">
+                        <path d="M8 0a7.992 7.992 0 0 0-6.583 12.535 1 1 0 0 0 .12.183l.12.146c.112.145.227.285.326.4l5.245 6.374a1 1 0 0 0 1.545-.003l5.092-6.205c.206-.222.4-.455.578-.7l.127-.155a.934.934 0 0 0 .122-.192A8.001 8.001 0 0 0 8 0Zm0 11a3 3 0 1 1 0-6 3 3 0 0 1 0 6Z"/>
+                    </svg>
+                   <span class="sr-only">Set location</span>
+               </button>
+               <button type="button" class="inline-flex justify-center items-center p-2 text-gray-500 rounded cursor-pointer hover:text-gray-900 hover:bg-gray-100 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-600">
+                   <svg class="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 18">
+                        <path d="M18 0H2a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2Zm-5.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Zm4.376 10.481A1 1 0 0 1 16 15H4a1 1 0 0 1-.895-1.447l3.5-7A1 1 0 0 1 7.468 6a.965.965 0 0 1 .9.5l2.775 4.757 1.546-1.887a1 1 0 0 1 1.618.1l2.541 4a1 1 0 0 1 .028 1.011Z"/>
+                    </svg>
+                   <span class="sr-only">Upload image</span>
+               </button>
+           </div>
+       </div>
+   </div>
+</form>
+<p class="ml-auto text-xs text-gray-500 dark:text-gray-400">Remember, contributions to this topic should follow our <a href="#" class="text-blue-600 dark:text-blue-500 hover:underline">Community Guidelines</a>.</p>
+
             </div>
         </div>
 
